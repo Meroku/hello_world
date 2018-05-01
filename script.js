d405b580547140a5a75058c9b8d3cadca479f789
Meroku <38563674+Meroku@users.noreply.github.com>
2018-05-01 19:56:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,16 +17,17 @@ var forSpeed;
 var wallActive = false;
 var walls = [];
 var vector = [];
-
+var it;
 var gravityOn = false;
-var dragOn = true;
 
 var clearCanv = true;
 
 var bigBalls = true;
 
+canvas.addEventListener('contextmenu', onContextMenu, false);
 document.addEventListener("keydown", keyDownHandler);
 canvas.addEventListener("mousedown", getPosition, false);
+//canvas.addEventListener("mousedown", goMoving, true);
 
 function clearCanvas() {                        // создание шарика
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -45,43 +46,108 @@ function getPosition(event) // ШАРИК
   y1 = y;
       if(wallActive == false){
   forSpeed = objArray.length;
-  objArray[objArray.length] = new Ball(x, y, randomRadius(radius));
+  if(!prov(event)){
+  objArray[objArray.length] = new Ball(x, y, randomRadius(radius));}
   var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-ctx.moveTo(x,y);
-ctx.lineWidth = 2; // толщина линии
-canvas.addEventListener("mouseup", write2, false);}
+//  ctx.beginPath();
+//  ctx.moveTo(x,y);
+//  ctx.lineWidth = 2; // толщина линии
+canvas.addEventListener('mousemove', function(e) {
+  var x=e.x;
+  var y=e.y;
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+canvas.addEventListener("mouseup", write2, false);
+}, false);
+}
 else{
   var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.moveTo(x,y);
-  ctx.lineWidth = 2; // толщина линии
-  canvas.addEventListener("mouseup", write2, false);
+  //ctx.beginPath();
+  //ctx.moveTo(x,y);
+  //ctx.lineWidth = 2; // толщина линии
+canvas.addEventListener('mousemove', function(e) {
+  var x=e.x;
+  var y=e.y;
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+canvas.addEventListener("mouseup", write2, false);
+}, false);
 }
 }
 else {onContextMenu();}
 }
 function write2(event) {
+if (event.which == 1) {
   var x=event.x;
   var y=event.y;
   x -= canvas.offsetLeft;
   y -= canvas.offsetTop;
 if(wallActive == false){
-  ctx.lineTo(x, y); //рисуем линию
-  ctx.stroke();
-  ctx.closePath();
+  //ctx.lineTo(x, y); //рисуем линию
+  //ctx.stroke();
+//  ctx.closePath();
   speedofBall(x1,y1,x,y,forSpeed);
   vector[vector.length] = new Vector(x1,y1,x,y);
 }
   else {
-    ctx.lineTo(x, y); //рисуем линию
-    ctx.stroke();
-    ctx.closePath();
+  //  ctx.lineTo(x, y); //рисуем линию
+  //  ctx.stroke();
+  //  ctx.closePath();
     walls[walls.length] = new Walls(x1,y1,x,y);
+  }
   }
 }
 
+function prov(e){
+  var x = e.x;
+  var y = e.y;
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  if(objArray.length != null){
+        for(i in objArray){
+        if(((x >= objArray[i].x && x <= objArray[i].x + radius) || (x <= objArray[i].x && x >= objArray[i].x - radius)) && ((y >= objArray[i].y - radius && y <= objArray[i].y) ||
+         (y >= objArray[i].y && y <= objArray[i].y + radius ))){return true; break;}
+       }}else {return false;}
+}
 //==========================
+function goMoving(e){
+  var x = e.x;
+  var y = e.y;
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+        for(i in objArray){
+        if(((x >= objArray[i].x && x <= objArray[i].x + radius) || (x <= objArray[i].x && x >= objArray[i].x - radius)) && ((y >= objArray[i].y - radius && y <= objArray[i].y) ||
+         (y >= objArray[i].y && y <= objArray[i].y + radius ))){
+           var mx=parseInt(e.clientX-canvas.offsetLeft);
+           var my=parseInt(e.clientY-canvas.offsetTop);
+
+           var ddx=mx-x;
+           var ddy=my-y;
+                //objArray[it].x +=ddx;
+                //objArray[it].y +=ddy;
+           draw();
+           x=mx;
+           y=my;
+         }}
+}
+ function myMove(e){
+      var mx=parseInt(e.clientX-offsetX);
+      var my=parseInt(e.clientY-offsetY);
+
+      var ddx=mx-startX;
+      var ddy=my-startY;
+           objArray[it].x+=ddx;
+           objArray[it].y+=ddy;
+      draw();
+      startX=mx;
+      startY=my;
+}
 
 //==========================
 var menu = document.querySelector('.menu');
@@ -97,20 +163,28 @@ function onContextMenu(e){
 e.preventDefault();
 var x = (e.layerX == undefined ? e.offsetX : e.layerX) + 1;
 var y = (e.layerY == undefined ? e.offsetY : e.layerY) + 1;
+if (wallActive == false) {
 for(var i in objArray){
-  if(((x >= objArray[i].x && x <= objArray[i].x + radius) || (x <= objArray[i].x && x >= objArray[i].x - radius)) || ((y >= objArray[i].y - radius && y <= objArray[i].y) ||
+  if(((x >= objArray[i].x && x <= objArray[i].x + radius) || (x <= objArray[i].x && x >= objArray[i].x - radius)) && ((y >= objArray[i].y - radius && y <= objArray[i].y) ||
    (y >= objArray[i].y && y <= objArray[i].y + radius ))){
 showMenu(objArray[i].x, objArray[i].y);
 forMenuBtn = i;
 canvas.addEventListener('mousedown', onMouseDown, false);
 break;
-}}}
+}}} else {
+  for(var i in walls){
+    if (((x >= walls[i].x1 && x <= walls[i].x2) || (x <= walls[i].x1 && x >= walls[i].x2)) && ((y >= walls[i].y1 && y <= walls[i].y2) || (y <= walls[i].y1 && x >= walls[i].y2))){
+  canvas.addEventListener('mousedown', onMouseDown, false);
+  showMenu(walls[i].x1, walls[i].y1);
+  forMenuBtn = i;
+}}
+}}
 function onMouseDown(e){
 hideMenu();
 document.removeEventListener('mousedown', onMouseDown);
 }
 
-canvas.addEventListener('contextmenu', onContextMenu, false);
+//canvas.addEventListener('contextmenu', onContextMenu, false);
 
 
 var button = document.getElementById('play');
@@ -137,6 +211,7 @@ chbtn.onclick = function() {
 var delbtn = document.getElementById('delete');
 delbtn.onclick = function() {
   objArray[forMenuBtn] = new Ball(0,0,0);
+//    walls[forMenuBtn] = new Wall(0,0,0);
   hideMenu();
 }
 var wallbtn = document.getElementById('Wall');
@@ -218,15 +293,84 @@ function drWallsCollision() {  // замени
         for (var obj2 in walls) {
             if ((distanceNextFrame2(objArray[obj1], walls[obj2], radius) == true)) {
                 var theta1 = objArray[obj1].angle();
-                var phi = Math.atan2(walls[obj2].y1 - objArray[obj1].y, walls[obj2].x1 - objArray[obj1].x);
+                var theta2 = walls[obj2].angle();
                 var m1 = objArray[obj1].mass;
                 var v1 = objArray[obj1].speed();
+                var sin = Math.abs(Math.sin(Math.abs(theta1) - Math.PI/2 - Math.abs(theta2)));
+                var cos = Math.abs(Math.cos(Math.abs(theta1) - Math.PI/2 - Math.abs(theta2)));;
+                var dx1;
+                var dy1;
+                if((Math.abs(Math.sin(theta1 + theta2)) >= Math.sin(Math.PI/2) - 0.08 ))
+                {objArray[obj1].dx = -objArray[obj1].dx;
+                objArray[obj1].dy = -objArray[obj1].dy;}
+                else if((Math.abs(Math.sin(theta1)) >= 0.08 && Math.abs(Math.sin(theta1)) <= 0.99)){
 
-                var dx1F = (v1 * Math.cos(theta1 - phi) * (m1/2)) / (2*m1) * Math.cos(phi) + v1*Math.sin(theta1-phi) * Math.cos(phi+Math.PI/2);
-                var dy1F = (v1 * Math.cos(theta1 - phi) * (m1/2)) / (2*m1) * Math.sin(phi) + v1*Math.sin(theta1-phi) * Math.sin(phi+Math.PI/2);
-
+                  if((theta1*Math.PI/180 > 0 && theta1*Math.PI/180 < 90) || (theta1*Math.PI/180 > 180 && theta1*Math.PI/180 < 270)){
+                if(Math.sin(theta2) <= 0.70 && Math.sin(theta2) >= -0.70){
+                objArray[obj1].dx = objArray[obj1].dx;
+                objArray[obj1].dy = -objArray[obj1].dy;
+              }
+                else if (Math.sin(theta2) >= 0.70 || Math.sin(theta2) <= -0.70) {
+                  objArray[obj1].dx = -objArray[obj1].dx;
+                  objArray[obj1].dy = objArray[obj1].dy;
+                }
+              } else if((theta1*Math.PI/180 > 90 && theta1*Math.PI/180 < 180) || (theta1*Math.PI/180 > -180 && theta1*Math.PI/180 < 0)){
+                if(Math.sin(theta2) <= 0.70 && Math.sin(theta2) >= -0.70){
+                objArray[obj1].dx = objArray[obj1].dx;
+                objArray[obj1].dy = -objArray[obj1].dy;
+              }
+                else if (Math.sin(theta2) >= 0.70 || Math.sin(theta2) <= -0.70) {
+                  objArray[obj1].dx = -objArray[obj1].dx;
+                  objArray[obj1].dy = objArray[obj1].dy;
+                }
+              }
+            }
+              else if(Math.abs(Math.sin(theta1)) >= 0.99){
+              //  if(){Math.abs(Math.sin(theta2)) <= 0.99 && Math.abs(Math.sin(theta2)) >= 0.08}
+            //else
+            if(Math.abs(Math.sin(theta2)) <= 0.70){
+              var bp = objArray[obj1].dx;
+              objArray[obj1].dx = -objArray[obj1].dy;
+              objArray[obj1].dy = bp;
+            }
+              else if (Math.abs(Math.sin(theta2)) >= 0.70) {
+                objArray[obj1].dx = -objArray[obj1].dx;
+                objArray[obj1].dy = objArray[obj1].dy;
+              }
+              if (Math.abs(Math.sin(theta2)) <= 0.08) {
                 objArray[obj1].dx = -objArray[obj1].dx;
                 objArray[obj1].dy = -objArray[obj1].dy;
+              }
+              else if (Math.abs(Math.sin(theta2)) <= 0.99) {
+                objArray[obj1].dx = -objArray[obj1].dx;
+                objArray[obj1].dy = -objArray[obj1].dy;
+              }
+            }
+            else if(Math.abs(Math.sin(theta1)) <= 0.08){
+            //  if(){Math.abs(Math.sin(theta2)) <= 0.99 && Math.abs(Math.sin(theta2)) >= 0.08}
+          //else
+          if(Math.abs(Math.sin(theta2)) <= 0.70){
+            var bp = objArray[obj1].dy;
+            objArray[obj1].dy = -objArray[obj1].dx;
+            objArray[obj1].dx = bp;
+          }
+            else if (Math.abs(Math.sin(theta2)) >= 0.70) {
+              objArray[obj1].dx = -objArray[obj1].dx;
+              objArray[obj1].dy = objArray[obj1].dy;
+            }
+            if (Math.abs(Math.sin(theta2)) <= 0.08) {
+              objArray[obj1].dx = -objArray[obj1].dx;
+              objArray[obj1].dy = -objArray[obj1].dy;
+            }
+            else if (Math.abs(Math.sin(theta2)) <= 0.99) {
+              objArray[obj1].dx = -objArray[obj1].dx;
+              objArray[obj1].dy = -objArray[obj1].dy;
+            }
+          }
+                else{
+                  objArray[obj1].dx = -objArray[obj1].dx;
+                  objArray[obj1].dy = -objArray[obj1].dy;
+                }
           }
                     }
 
